4f725072b9053cce643ea4f4ec8ae5fc425df462
igoryan <nkulinigor1984@icloud.com>
2022-05-14 19:49:33 +0200

    task_001

--- task_001/Program.cs
+++ task_001/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Найти сумму чисел от 1 до А
 
-
+Console.Write("Введи число - ");
+string writeA = Console.ReadLine();
+int A = Convert.ToInt32(writeA);
+int GetNumbersSum(int A)
+{
+    int number = 1;
+    int sum = 0;
+    for (int i = 0; i < A; i++)
+    {
+        sum = sum + number;
+        number++;
+    }
+    return sum;
+}
+int NumbersSum = GetNumbersSum(A);
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {NumbersSum}");
